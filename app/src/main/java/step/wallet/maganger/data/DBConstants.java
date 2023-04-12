594fbc445c428b4f3b5a8ff2fd66644f7b0f5aa9
Krok <kadetkroksapsp2@gmail.com>
2023-04-12 18:34:33 +0200

    initial commit

--- app/src/main/java/step/wallet/maganger/data/DBConstants.java
+++ app/src/main/java/step/wallet/maganger/data/DBConstants.java
@@ -21,12 +21,14 @@ public class DBConstants {
     static final String COL_CAT_ARCHIVED = "CATEGORY_ARCHIVED";
 
     static final String COL_SUBCAT_ID = "SUBCATEGORY_ID";
-    static final String COL_SUBCAT_SUPERCAT_ID= "SUPERCATEGORY_ID";
+    static final String COL_SUBCAT_SUPERCAT_ID = "SUPERCATEGORY_ID";
     static final String COL_SUBCAT_NAME = "INFO_FIELD_TEXT";
 
     static final String COL_ACC_ID = "ACCOUNT_ID";
     static final String COL_ACC_NAME = "ACCOUNT_NAME";
     static final String COL_ACC_TYPE = "ACCOUNT_TYPE";
+    static final String COL_ACC_CURRENCY = "ACCOUNT_CURRENCY";
+    static final String COL_ACC_DESC = "ACCOUNT_DESCRIPTION";
     static final String COL_ACC_BALANCE = "ACCOUNT_BALANCE";
 
     static final String COL_TRANSACTION_ID = "TRANSACTION_ID";
@@ -35,7 +37,7 @@ public class DBConstants {
     static final String COL_TRANSACTION_ID_SUBCAT = "TRANSACTION_ID_SUBCATEGORY";
     static final String COL_TRANSACTION_DATE = "TRANSACTION_ID_DATETIME";
     static final String COL_TRANSACTION_ID_ACC = "TRANSACTION_ID_ACCOUNT";
-    static final String COL_TRANSACTION_CURRENCY= "TRANSACTION_CURRENCY";
+    static final String COL_TRANSACTION_CURRENCY = "TRANSACTION_CURRENCY";
     static final String COL_TRANSACTION_NOTE_1 = "TRANSACTION_NOTE_1";
     static final String COL_TRANSACTION_NOTE_2 = "TRANSACTION_NOTE_2";
     static final String COL_TRANSACTION_PHOTO = "TRANSACTION_PHOTO";
@@ -72,8 +74,8 @@ public class DBConstants {
     static {
         DATABASE_CREATE_3 = "CREATE TABLE IF NOT EXISTS "
                 + TABLE_ACCOUNT
-                + " (" + COL_ACC_ID + " INTEGER PRIMARY KEY, " + COL_ACC_NAME + " TEXT, " + COL_ACC_TYPE + " TEXT, " +
-                COL_ACC_BALANCE + " REAL);";
+                + " (" + COL_ACC_ID + " INTEGER PRIMARY KEY, " + COL_ACC_NAME + " TEXT, " + COL_ACC_TYPE + " TEXT, "
+                + COL_ACC_CURRENCY + " TEXT, " + COL_ACC_DESC + " TEXT, " + COL_ACC_BALANCE + " REAL);";
     }
 
     static {
